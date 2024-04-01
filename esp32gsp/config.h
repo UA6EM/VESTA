1819261a065dab1bae8aa0809b0beff605c6cad5
Victor <ua6em@yandex.ru>
2024-04-01 12:40:21 +0300

    Update config.h

--- esp32gsp/config.h
+++ esp32gsp/config.h
@@ -2,5 +2,5 @@
 #include <Arduino.h>
 #include "config.h"
 
-const char * ssid = "Open";             // Название WIFI сети
+const char * ssid = "OpenNet";             // Название WIFI сети
 const char * password = "12345678";     // Пароль от WIFI сети не менее 8-и символов
