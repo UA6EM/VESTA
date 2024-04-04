aa324b13920813cb9ff1b877c8a7286977edf26e
Victor <ua6em@yandex.ru>
2024-04-04 19:40:27 +0300

    Add files via upload

---
+++ esp32_mcp41x_sqlite3/config.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <Arduino.h>
+#include "config.h"
+
+const char * ssid = "OpenNet";             // Название WIFI сети
+const char * password = "12345678";     // Пароль от WIFI сети не менее 8-и символов
